691c29b89ccfcc9ff9970f1c89b0173161b26743
DS <cckn1985@gmail.com>
2018-09-03 03:13:29 +0900

    nodebird

--- nodebird/nodebird-api/routes/middlewares.js
+++ nodebird/nodebird-api/routes/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -19,7 +20,7 @@ exports.isNotLoggedIn = (req, res, next) => {
 exports.verifyToken = (req, res, next) => {
   try {
     req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-    return next()
+    return next();
   } catch (error) {
     if (error.name === 'TokenExpiredError') {
       return res.status(419).json({
@@ -32,4 +33,23 @@ exports.verifyToken = (req, res, next) => {
       message: '유효하지 않은 토큰입ㄴ디ㅏ.',
     });
   }
+};
+
+exports.apiLimiter = new RateLimit({
+  windowMs: 1000,
+  max: 1,
+  delayMs: 0,
+  handler(req, res) {
+    res.status(this.statusCode).json({
+      code: this.statusCode,
+      message: '1분에 한 번만 요청할 수 있습니다.',
+    });
+  },
+});
+
+exports.deprecated = (req, res) => {
+  res.status(410).json({
+    code: 410,
+    message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.',
+  });
 };
